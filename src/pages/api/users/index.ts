38201c0c2800c196d0ef6ce3b6af0d62843992b0
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-04-29 12:18:03 -0300

    config prisma CLI , and create first model Users

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -1,10 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { Users } from '@/utils/users'
 
+// GET all users
 async function handlerGET(req: NextApiRequest, res: NextApiResponse) {
   return res.json({ Users })
 }
 
+// POST all users
 async function handlerPOST(req: NextApiRequest, res: NextApiResponse) {
   return res.json({ created: true })
 }
